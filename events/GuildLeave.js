acc5ed71842dd927f78c825c82b0a6c01dbe29ce
Yash <yashbaruah@hotmail.com>
2024-09-15 21:27:21 -0500

    Revert "Add files via upload"
    This reverts commit 9a46ae769332b71d745b2f0d4fe603d73f08a1c6.

--- events/GuildLeave.js
+++
@@ -1,8 +0,0 @@
-const { Events } = require('discord.js');
-
-module.exports = {
-    name: Events.GuildDelete,
-    async execute(client, guild) {
-        console.log(`Left guild ${guild.name} with ${guild.memberCount} members!`);
-    }
-}
