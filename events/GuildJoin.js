238acd6c6e3b8158eee557405c65a34e6dc55da1
kennnyq <kennyq6596@gmail.com>
2024-09-15 21:25:44 -0500

    Revert "Add files via upload"
    This reverts commit 9a46ae769332b71d745b2f0d4fe603d73f08a1c6.

--- events/GuildJoin.js
+++
@@ -1,8 +0,0 @@
-const { Events } = require('discord.js');
-
-module.exports = {
-    name: Events.GuildCreate,
-    async execute(client, guild) {
-        console.log(`Joined guild ${guild.name} with ${guild.memberCount} members!`);
-    }
-}
--- schemas/test.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-let test = new Schema({
-    Content: String
-});
-
-module.exports = model('testingschema1912323131', test);
