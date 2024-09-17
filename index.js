f681ef89c4ea444cdd8fc5733c578556cb91955e
kennnyq <kennyq6596@gmail.com>
2024-09-16 20:04:03 -0500

    Experimenting with Discord API

--- index.js
+++ index.js
@@ -1,16 +1,187 @@
-const { Client, PermissionsBitField } = require('discord.js');
-const mongoose = require('mongoose');
+// Import the discord.js module
+// const { Client, PermissionsBitField } = require('discord.js'); // Old import statement
+const { Client, GatewayIntentBits, } = require('discord.js');
 
+// Create a new client instance
 const client = new Client({
     intents: [
-        'Guilds',
-        'GuildMembers'
+        GatewayIntentBits.Guilds,        // Gives access to server operations (such as bot joins/leaves, guild updates, etc.).
+        GatewayIntentBits.GuildMessages, // Allows access to receive events related to messages sent in text channels in (servers).
+        GatewayIntentBits.MessageContent // Allows the bot to access the content of messages in servers.
     ]
 });
 
-client.config = require('./config.json');
+// Configuration stuff, not sure what it all does yet
+client.config = require('./config.json'); // Gets config options from config.json (like bot token)
 client.cooldowns = new Map();
 client.cache = new Map();
+
+// Login to Discord with your app's token
+console.log(`Logging in...`);
+client.login(client.config.TOKEN);
+client.on('ready', function () {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    // Not sure what this line does: commented out for now
+	// require('./utils/CheckIntents.js')(client);
+});
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+// Listen for messages and respond
+client.on('messageCreate', (message) => {
+    console.log('messageCreate Event: ', message)
+    // Here, if you send "!ping", the bot will respond with "Pong!"
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+    if (message.content === 'I love!') {
+        message.channel.send('Cats!');
+    }
+});
+
+
+// Adding slash commands
+
+// API imports needed to register commands with discord
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+
+// Tools needed to create buttons with discord API
+const { ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
+
+// Define the slash commands
+const commands = [
+    {
+        name: 'poke',
+        description: 'Poke the bot! (Send a message with a button)'
+    },
+    {
+        name: 'ping',
+        description: 'Replies with Pong!'
+    },
+    {
+        name: 'clicker',
+        description: 'Starts a clicker button thing'
+    }
+];
+
+// Register the slash commands (I don't know what this does)
+const rest = new REST({ version: '10' }).setToken(client.config.TOKEN);
+
+// When the client is ready, register the commands
+client.once('ready', async () => {
+
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    // Not entirely sure what this means, but apparently
+    // it sends the commands from the const variable above
+    // into discord's servers.
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        // Register commands globally (for all servers)
+        await rest.put(
+            Routes.applicationCommands(client.user.id),
+            { body: commands }
+        );
+
+        // Ignore for now ..
+        await rest.put(
+            Routes.applicationGuildCommands(client.user.id, '1281840845800345611'),
+            { body: commands }
+        );
+        await rest.put(
+            Routes.applicationGuildCommands(client.user.id, '1103836999732969532'),
+            { body: commands }
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        // If the above code somehow fails, log the error
+        console.error(error);
+    }
+});
+
+// Now that we have the commands in the bot, we can
+// start listening to them. This event listener does that:
+client.on('interactionCreate', async interaction => {
+    // Command handler
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+    if (commandName == 'poke') {
+        // Create a button
+        const button = new ButtonBuilder()
+            .setCustomId('poke_button')
+            .setLabel('Poke me!')
+            .setStyle(ButtonStyle.Primary); // Blue button
+
+        // Create an action row to hold the button
+        const row = new ActionRowBuilder().addComponents(button);
+
+        // Reply with the button
+        await interaction.reply({
+            content: 'Poke me!', // Text row
+            components: [row]    // Action Row
+        });
+    }
+    if (commandName == 'clicker') {
+        const button = new ButtonBuilder()
+            .setCustomId('clicker_button')
+            .setLabel('Click me!')
+            .setStyle(ButtonStyle.Primary); // Blue button
+        
+        // Create an action row to hold the button
+        const row = new ActionRowBuilder().addComponents(button);
+
+        let clickCount = 0;
+
+        // Initial user response
+		const message = await interaction.reply({
+			content: 'Click the button!', // Text Row
+			components: [row],            // Action Row
+		});
+
+        // We use a "collector" to listen to button clicks
+        // Not really sure what a collector is, or why it is used here
+        const collector = message.createMessageComponentCollector({
+            // This filter means that we only listen to clicks
+            // from buttons with the 'clicker_button' id
+            filter: i => i.customId === 'clicker_button',
+            // Collector will stop after 60 seconds
+            time: 60000
+        });
+
+        // Event Listener for our clicker collector
+        collector.on('collect', async i => {
+			clickCount++;
+			await i.update({
+				content: `You clicked the button ${clickCount} times!`
+			});
+		});
+    }
+
+});
+
+client.on('interactionCreate', async interaction => {
+    // Button Handler
+    if (!interaction.isButton()) return;
+
+    if (interaction.customId === 'poke_button') {
+        await interaction.reply('Ouch that hurts!');
+    }
+})
+// Blah blah blah
+
+/*
 
 // Each of these exports a function, it's the same as doing
 // const ComponentLoader = require('./utils/ComponentLoader.js');
@@ -27,20 +198,15 @@ require('./utils/RegisterCommands.js')(client);
 // This way we can connect to the database while waiting for discord to respond
 // [Application startup] -> [Database connection] -> [client.login()] -> [Discord responds] -> [Ready event]
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const mongoose = require('mongoose');
 ( async function() {
 	if (!client.config.mongoURL) return console.warn('MongoDB URL is not provided in the config.json file, skipping database connection...');
 	await mongoose.connect(client.config.mongoURL);
 })();
 
-console.log(`Logging in...`);
-client.login(client.config.TOKEN);
-client.on('ready', function () {
-    console.log(`Logged in as ${client.user.tag}!`);
-
-	require('./utils/CheckIntents.js')(client);
-});
-
 client.on('messageCreate', () => {} )
+
+*/
 
 async function InteractionHandler(interaction, type) {
 
@@ -104,6 +270,8 @@ async function InteractionHandler(interaction, type) {
     }
 }
 
+/*
+
 ////////////////////////////////////////////////////////////////
 // These are all the entry points for the interactionCreate event.
 // This will run before any command processing, perfect for logs!
@@ -130,3 +298,5 @@ client.on('interactionCreate', async function(interaction) {
     if (!interaction.isModalSubmit()) return;
     await InteractionHandler(interaction, 'modals');
 });
+
+*/
